7c42f0732e78825d12e043b50e980646e4e4df7f
Tricia Amheiser <tricia@taarts.net>
2022-08-04 16:17:29 -0400

    Added default values

--- Controllers/DiceController.cs
+++ Controllers/DiceController.cs
@@ -12,7 +12,7 @@ namespace BasicAPI.Controllers
     public class DiceController : ControllerBase
     {
         [HttpGet("{sides}")]
-        public List<int> Roll(int sides, int count)
+        public List<int> Roll(int sides, int count = 1)
         {
             var rolls = new List<int>();
 
